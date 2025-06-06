b782deeb9a6b70d99cea002caea2c162673cc385
Abhishekhivarkar <abhihivarkar783@gmail.com>
2025-06-06 17:30:45 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,21 +1,3 @@
-
-# from django.urls import path
-# from . import views
-# from django.contrib.auth import views as auth_views
-
-# urlpatterns = [
-#     path('', views.home_view, name='home'),  # new home page
-#     path('feed/', views.feed_view, name='feed'),
-#     path('register/', views.register_view, name='register'),
-#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-#     path('login/', views.CustomLoginView.as_view(), name='login'),
-
-#     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-#     path('profile/<str:username>/', views.profile_view, name='profile'),
-#     path('post/<int:post_id>/', views.post_detail_view, name='post_detail'),
-#     path('upload/', views.upload_post, name='upload_post'),
-#     path('post/<int:post_id>/like/', views.toggle_like, name='toggle_like'),
-# ]
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
