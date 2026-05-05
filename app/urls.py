b0db1295e17b5c47158d0f1a230aef9e6f422487
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-05-05 22:51:05 +0530

    Fix register path in urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('feed/', views.feed_view, name='feed'),
-    path('register/', views.register_view, name='register'),
+    path('register/', views.register_view, name='register'), 
     path('accounts/login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html', next_page='login'), name='logout'),
     path('search/', views.user_search_view, name='user_search'),
